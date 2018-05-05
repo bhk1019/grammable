16bff07a9b627c0305b3d4b2fa8c7ec06a58281f
Ben Kim <bhk1019@gmail.com>
2018-05-05 04:09:10 +0000

    installed devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # devise mailer confic
+  config.action_mailer.default_url_options = { host: 'grammable-ben-kim.herokuapp.com' }
 end
